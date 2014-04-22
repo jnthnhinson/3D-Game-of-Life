cb51ecee24a3796bd3a26bfac5d90c3ff4777538
jnthnhinson <jnthnhinson@gmail.com>
2014-04-21 19:34:46 -0500

    first commit

---
+++ src/MouseMapper.java
@@ -0,0 +1,42 @@
+// Taken from http://www.jpct.net/wiki/index.php/Advanced_example#Key-_and_mouse_mapping
+
+
+public static class MouseMapper {
+
+	private boolean hidden = false;
+
+	private int height = 0;
+
+	public MouseMapper(FrameBuffer buffer) {
+		height = buffer.getOutputHeight();
+		init();
+	}
+
+	public void hide() {
+		if (!hidden) {
+			Mouse.setGrabbed(true);
+			hidden = true;
+		}
+	}
+
+	//.....
+
+	public int getDeltaY() {
+		if (Mouse.isGrabbed()) {
+			return Mouse.getDY();
+		} else {
+			return 0;
+		}
+	}
+
+	private void init() {
+		try {
+			if (!Mouse.isCreated()) {
+				Mouse.create();
+			}
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
