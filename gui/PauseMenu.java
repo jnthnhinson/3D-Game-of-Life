678ce1c7bda075f388aebf7d2637ea79b761c62d
hunt210 <hunter2_10@yahoo.com>
2014-04-21 22:21:15 -0500

    Here's some of the starting GUI

---
+++ gui/PauseMenu.java
@@ -0,0 +1,14 @@
+package gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class PauseMenu extends JFrame{
+	
+	public PauseMenu(){
+		this.setPreferredSize(new Dimension(1000,750));
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ gui/StevesWorld.java
@@ -0,0 +1,17 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class StevesWorld extends JPanel implements ActionListener{
+
+	public StevesWorld(){
+		this.addKeyListener(new KeyHandler());
+	}
+	public void actionPerformed(ActionEvent e){
+		repaint();
+	}
+}
