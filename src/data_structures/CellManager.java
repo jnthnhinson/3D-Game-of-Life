ece7a7ffdf8aa33c6acbd475c80eef464653f482
Shrumdog <ian.shrum@gmail.com>
2014-04-23 14:27:59 -0500

    added the Stack that I forgot in CellManager

--- src/data_structures/CellManager.java
+++ src/data_structures/CellManager.java
@@ -2,18 +2,21 @@ package src.data_structures;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Stack;
 
 import com.threed.jpct.Object3D;
 
 public class CellManager {
 	
 	private ArrayList<ArrayList<ArrayList<Cell>>> grid;
+	private Stack<Cell> updates;
 	private Object3D template;
 	private Random rand;
 	private int size;
 	
 	public CellManager(int size){
 		grid = new ArrayList<ArrayList<ArrayList<Cell>>>();
+		updates = new Stack<Cell>();
 		rand = new Random();
 		this.size = size;
 		
@@ -21,18 +24,18 @@ public class CellManager {
 		initializePopulation();
 	}
 	
-	private ArrayList<Cell> getNeighbors(Cell cell){
-		ArrayList<Cell> ret = new ArrayList<Cell>();
+	private void getNeighbors(Cell cell){
 		int[] coor = cell.getCoordinates();
 		for(int x = coor[0] - 1; x <= coor[0] + 1; x++){
 			for(int y = coor[1] - 1; y <= coor[1] + 1; y++){
 				for(int z = coor[2] - 1; z <= coor[2] + 1; z++){
 					int[] curCoor = {x, y, z};
-					if(inRange(curCoor))ret.add(getCell(curCoor));
+					if(inRange(curCoor) && !coorEquivalence(coor, curCoor)){
+						updates.push(getCell(curCoor));
+					}
 				}
 			}
 		}
-		return ret;
 	}
 	
 	private void initializePopulation(){
@@ -60,6 +63,12 @@ public class CellManager {
 	
 	private Cell getCell(int[] coordinates){
 		return grid.get(coordinates[0]).get(coordinates[1]).get(coordinates[2]);
+	}
+	
+	private boolean coorEquivalence(int[] x, int[] y){
+		for(int n = 0; n < x.length; n++){
+			if(x[n] != y[n]){return false;}
+		} return true;
 	}
 	
 	private int generateIndex(){
