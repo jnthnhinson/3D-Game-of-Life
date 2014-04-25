c9fb5a93e543e609ab8f28d74b79d2cf5fc3e8c8
zeebenzene <zeebenzene@gmail.com>
2014-04-24 22:36:17 -0500

    Same with godCamera

--- src/graphics/GodCamera.java
+++ src/graphics/GodCamera.java
@@ -9,83 +9,28 @@ public class GodCamera extends GameCamera{
 	//private final static SimpleVector ELLIPSOID_RADIUS = new SimpleVector(COLLISION_SPHERE_RADIUS,PLAYER_HEIGHT/2f,COLLISION_SPHERE_RADIUS);
 	private final static float PLAYER_HEIGHT = 30f;
 
-	
-	private boolean left = false;
-	private boolean right = false;
-	private boolean up = false;
-	private boolean down = false;
-	private boolean forward = false;
-	private boolean backward = false;
-	private boolean jumping = false;
-	private boolean falling = false;
-	private boolean strafeLeft = false;
-	private boolean strafeRight = false;
-	private boolean tiltLeft = false;
-	private boolean tiltRight = false;
-	private boolean zoomOut = false;
-	
-	
-
 	public GodCamera(World world) {
 		super(world);
 		// TODO Auto-generated constructor stub
 	}
 
 	public void updateStatus(String dir) {
-		if(dir == "L")			{strafeLeft = true;}
-		else if(dir == "R")		{strafeRight = true;}
-		else if(dir == "F")		{forward = true;}
-		else if(dir == "B")		{backward = true;}
-		else if(dir == "U")		{jumping = true;}
-		//else if(dir == "D")	{zoomOut = true;}
-		else if(dir == "TL")	{tiltLeft = true;}
-		else if(dir == "TR")	{tiltRight = true;}
+		if(dir == "L"){moveCamera(Camera.CAMERA_MOVELEFT, 1);}
+		else if(dir == "R")		{moveCamera(Camera.CAMERA_MOVERIGHT, 1);}
+		else if(dir == "F")		{moveCamera(Camera.CAMERA_MOVEIN, 1);}
+		else if(dir == "B")		{moveCamera(Camera.CAMERA_MOVEOUT, 1);}
+		else if(dir == "U")		{moveCamera(Camera.CAMERA_MOVEUP, 1);}
+		else if(dir == "D")		{moveCamera(Camera.CAMERA_MOVEDOWN, 1);}
+		else if(dir == "TL")	{tiltCamera("L");}
+		else if(dir == "TR")	{tiltCamera("R");}
 	}
-	
-	public void performMovement() {
-		performOtherMovement();
-		resetBools();
-	}
-	
-	/*
-	private void performOtherMovement() {
-		boolean cameraChanged = false;
-
-		if (forward) {walk(true);cameraChanged = true;}
-		else if (backward) {walk(false);cameraChanged = true;}
-		if (left) {turn(false);}
-		if (right) {turn(true);}
-		if (up) {tilt(true);}
-		if (down) {tilt(false);}
-		if (strafeLeft) {strafe(false); cameraChanged = true;}
-		if (strafeRight) {strafe(true); cameraChanged = true;}
-		if (cameraChanged) {moveCamera(new SimpleVector(0, -1, 0), PLAYER_HEIGHT/2f);}
-	}
-	*/
-	
-	public void performOtherMovement() {
-		if(strafeLeft){moveCamera(Camera.CAMERA_MOVELEFT, 1);}
-		else if(strafeRight){moveCamera(Camera.CAMERA_MOVERIGHT, 1);}
-		else if(forward){moveCamera(Camera.CAMERA_MOVEIN, 1);}
-		else if(backward){moveCamera(Camera.CAMERA_MOVEOUT, 1);}
-		else if(jumping){moveCamera(Camera.CAMERA_MOVEUP, 1);}
-		else if(falling){moveCamera(Camera.CAMERA_MOVEDOWN, 1);}
+	public void tiltCamera(String dir){
+		if(dir == "L"){
+			rotateCameraZ((float)-.03);
+		} else if(dir == "R"){
+			rotateCameraZ((float).03);
 		}
-	
-	public void resetBools() {
-		left = false;
-		right = false;
-		up = false;
-		down = false;
-		forward = false;
-		backward = false;
-		jumping = false;
-		falling = false;
-		strafeLeft = false;
-		strafeRight = false;
-		tiltLeft = false;
-		tiltRight = false;
-		zoomOut = false;
 	}
+
 	
 }
