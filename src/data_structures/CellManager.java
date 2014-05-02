85b783d2f9e919fdfea709d09f6fd2aa411d534f
Shrumdog <ian.shrum@gmail.com>
2014-05-01 21:22:54 -0500

    I'm Ian!

--- src/data_structures/CellManager.java
+++ src/data_structures/CellManager.java
@@ -12,7 +12,7 @@ public class CellManager {
 	private Stack<Cell> updates;
 	private World world;
 	private Random rand;
-	private int size;
+	private int size, cellSize;
 	private RandomColor getColor;
 
 	public CellManager(int size, World w){
@@ -21,17 +21,19 @@ public class CellManager {
 		updates = new Stack<Cell>();
 		rand = new Random();
 		this.size = size;
+		this.cellSize = CellSize.LARGE.getSize();
 		this.world = w;
-
+		
 		populateGrid();
 		populateWorld();
+		InitialConditions.GRID.initialize(this);
 	}
 
 	private void populateGrid(){
 		for(int x = 0; x < size; x ++){
 			for(int y = 0; y < size; y++){
 				for(int z = 0; z < size; z++){
-					Cell c = new Cell(CellSize.SMALL.getSize());
+					Cell c = new Cell(cellSize);
 					System.out.println(x + " : " + y + " : " + z);
 					this.grid[x][y][z] = c;
 					c.setCoordinates(x, y, z);
@@ -41,7 +43,7 @@ public class CellManager {
 	}
 
 	private void populateWorld(){
-		float dist = CellSize.SMALL.getSize() * (float)2.3;
+		float dist = cellSize * (float)2;
 		float newx = 0; float newy = 0; float newz = 0;
 		Cell c;
 
@@ -63,15 +65,16 @@ public class CellManager {
 		}
 	}
 
-
-	private void pushNeighbors(Cell cell){
+	private void handleNeighbors(Cell cell, boolean bool){
 		int[] coor = cell.getCoordinates();
 		for(int x = coor[0] - 1; x <= coor[0] + 1; x++){
 			for(int y = coor[1] - 1; y <= coor[1] + 1; y++){
 				for(int z = coor[2] - 1; z <= coor[2] + 1; z++){
 					int[] curCoor = {x, y, z};
-					if(inRange(curCoor) && !coorEquivalence(coor, curCoor)){
-						updates.push(getCell(x, y, z));
+					if(inRange(curCoor) && !coorEquivalence(coor, curCoor) && !updates.contains(getCell(x, y, z))){
+						Cell cur = getCell(x, y, z);
+						if(bool){updates.push(cur);}
+						cur.incPop(bool);
 					}
 				}
 			}
@@ -86,7 +89,7 @@ public class CellManager {
 				for(int z = coor[2] - 1; z <= coor[2] + 1; z++){
 					int[] curCoor = {x, y, z};
 					if(inRange(curCoor) && !coorEquivalence(coor, curCoor) && isCorner(coor, curCoor) 
-							&& getCell(x, y, z).isDead(cell)){
+							&& getCell(x, y, z).isAlive()){
 						num++;
 					}
 				}
@@ -103,7 +106,7 @@ public class CellManager {
 				for(int z = coor[2] - 1; z <= coor[2] + 1; z++){
 					int[] curCoor = {x, y, z};
 					if(inRange(curCoor) && !coorEquivalence(coor, curCoor) && !isCorner(coor, curCoor) 
-							&& getCell(x, y, z).isAlive(cell)){
+							&& getCell(x, y, z).isAlive()){
 						num++;
 					}
 				}
@@ -113,108 +116,90 @@ public class CellManager {
 	}
 
 	int totalNeighbors(Cell cell){
-		int num = 0;
-		num = numFlatNeighbors(cell) + numCornerNeighbors(cell);
-		return num;
+		return cell.numNeighbors();
 	}
 
 
-	public void rules() {
+//	public void rules() {
+//		Cell c;
+//		while(true){
+//			for (int x = 0; x < grid[0].length; ++x ){
+//				for (int y = 0; y < grid[0].length; y++){
+//					for (int z = 0; z < grid[0].length; z++){
+//						c = getCell(x, y, z);
+//						if (totalNeighbors(c) <= 1 && c.isDead(c)) {
+//							c.murderCell(c);
+//						}
+//						else if (totalNeighbors(c) >= 5 && totalNeighbors(c) < 8) {
+//							c.birthCell(c);
+//						}
+//						else if (totalNeighbors(c) >= 8) {
+//							c.murderCell(c);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+
+	private boolean isCorner(int[] ori, int[] cur){
+		return (((ori[0] - cur[0]) * (ori[1] - cur[1]) * (ori[2] - cur[2])) % 2 != 0);
+	}
+
+	private boolean inRange(int[] coordinates){
+		for(int n : coordinates){
+			if(n < 0 || n >= size){return false;}
+		} return true;
+	}
+
+	public Cell getCell(int x, int y, int z){
+		return grid[x][y][z];
+	}
+
+	private boolean coorEquivalence(int[] x, int[] y){
+		for(int n = 0; n < x.length; n++){
+			if(x[n] != y[n]){return false;}
+		} return true;
+	}
+	
+	public int[] generateCoor(){
+		int[] cur = {generateIndex(), generateIndex(), generateIndex()};
+		return cur;
+	}
+
+	private int generateIndex() {
+		return (int) Math.floor(rand.nextDouble()*size);
+	}
+	
+	public void update(){
+		Cell c;
+		boolean cond;
+		while(!updates.isEmpty()){
+			c = updates.pop();
+			cond = Rules.THREEDGROWTH.apply(this, c);
+			handleNeighbors(c, cond);
+		}
+	}
+	
+	public int getSize(){
+		return size;
+	}
+
+	public void seizurePlease() {
 		Cell c;
 		while(true){
 			for (int x = 0; x < grid[0].length; ++x ){
 				for (int y = 0; y < grid[0].length; y++){
 					for (int z = 0; z < grid[0].length; z++){
 						c = getCell(x, y, z);
-						if (totalNeighbors(c) <= 1 && c.isDead(c)) {
-							c.murderCell(c);
+
+						if (c.isAlive()) {
+							c.setAdditionalColor(getColor.randomColor());	
 						}
-						else if (totalNeighbors(c) >= 5 && totalNeighbors(c) < 8) {
-							c.birthCell(c);
-						}
-						else if (totalNeighbors(c) >= 8) {
-							c.murderCell(c);
-						}
+//						rules(c);
 					}
 				}
 			}
 		}
 	}
-	
-			private int numNeighbors(Cell cell){
-				int[] coor = cell.getCoordinates();
-				int num = 0;
-				for(int x = coor[0] - 1; x <= coor[0] + 1; x++){
-					for(int y = coor[1] - 1; y <= coor[1] + 1; y++){
-						for(int z = coor[2] - 1; z <= coor[2] + 1; z++){
-							int[] curCoor = {x, y, z};
-							if(inRange(curCoor) && !coorEquivalence(coor, curCoor) && getCell(x, y, z).isDead(cell)){
-								num++;
-							}
-						}
-					}
-				}
-				return num;
-			}
-
-			private boolean isCorner(int[] ori, int[] cur){
-				return (((ori[0] - cur[0]) * (ori[1] - cur[1]) * (ori[2] - cur[2])) % 2 != 0);
-			}
-
-			private boolean inRange(int[] coordinates){
-				for(int n : coordinates){
-					if(n < 0 || n >= size){return false;}
-				} return true;
-			}
-
-			public Cell getCell(int x, int y, int z){
-				return grid[x][y][z];
-			}
-
-			private boolean coorEquivalence(int[] x, int[] y){
-				for(int n = 0; n < x.length; n++){
-					if(x[n] != y[n]){return false;}
-				} return true;
-			}
-
-			private int generateIndex() {
-				return (int) Math.floor(rand.nextDouble()*size);
-			}
-
-			private void rules(Cell c) {
-				if (totalNeighbors(c) <= 1 && c.isDead(c)) {
-					c.murderCell(c);
-				}
-				else if (totalNeighbors(c) >= 5 && totalNeighbors(c) < 8) {
-					c.birthCell(c);
-				}
-				else if (totalNeighbors(c) >= 8) {
-					c.murderCell(c);
-				}
-				else if (numFlatNeighbors(c) > 4) {
-					c.murderCell(c);
-				}
-			}
-			
-			public void seizurePlease() {
-				Cell c;
-				while(true){
-					for (int x = 0; x < grid[0].length; ++x ){
-						for (int y = 0; y < grid[0].length; y++){
-							for (int z = 0; z < grid[0].length; z++){
-								c = getCell(x, y, z);
-								
-								if (y % 2 == 0 || z % 2 != 0 ) {
-									c.birthCell(c);
-									c.setVisibility(true);
-								}
-								if (c.isAlive(c)) {
-									c.setAdditionalColor(getColor.randomColor());	
-								}
-								rules(c);
-								}
-							}
-						}
-					}
-				}
-			}
+}
